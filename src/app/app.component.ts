fcfca9639a44b09edc023011c05034d76f6f15b4
HabeebR07 <habeebrahmanca07@gmail.com>
2024-12-19 23:33:20 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ButtonModule } from 'primeng/button';
+import { NavbarComponent } from "./components/layout/navbar/navbar.component";
+import { FooterComponent } from "./components/layout/footer/footer.component";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
